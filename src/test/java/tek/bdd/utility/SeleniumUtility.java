25e6ad88c09b03b63cfac5f1ba8ee07ea00b4b87
Elenika <1991pugacheva@gmail.com>
2024-07-30 21:12:12 -0400

    activity

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -1,10 +1,9 @@
-package tek.bdd.base.utility;
-
+package tek.bdd.utility;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import tek.bdd.base.base.BaseSetup;
+import tek.bdd.base.BaseSetup;
 
 import java.time.Duration;
 
@@ -12,12 +11,18 @@ public class SeleniumUtility extends BaseSetup {
     private WebDriverWait getWait(){
         return new WebDriverWait(getDriver(),Duration.ofSeconds(20));
     }
+    private WebElement waitForVisibility(By locator){
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
     //create a method to click on a given locator
     public void clickToElement(By locator){
         getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
     public  void sendText(By locator, String value){
-        getWait().until(ExpectedConditions.elementToBeClickable(locator)).sendKeys(value);
+        waitForVisibility(locator).sendKeys(value);
     }
-
+    // create method for getting the text of a locator
+    public String getElementText(By locator){
+    return waitForVisibility(locator).getText();
+    }
 }
