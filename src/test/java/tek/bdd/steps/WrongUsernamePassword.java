73500abec573342d32b8585b6914a2cf21e080ef
Elenika <1991pugacheva@gmail.com>
2024-07-31 22:17:48 -0400

    wrong email and password

---
+++ src/test/java/tek/bdd/steps/WrongUsernamePassword.java
@@ -0,0 +1,30 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import tek.bdd.pages.HomePage;
+import tek.bdd.pages.SignInPage;
+import tek.bdd.utility.SeleniumUtility;
+
+public class WrongUsernamePassword extends SeleniumUtility {
+
+    @Then("validate user is in sign in page")
+    public void validateSignInPage() {
+        String pageSubTitle = getElementText(SignInPage.PAGE_SUBTITLE);
+        Assert.assertEquals("Sign in", pageSubTitle);
+    }
+    @Then("enters \"invalid@gmail.com\" and \"Password@123\"")
+    public void user_enter_username_and_password() {
+        sendText(SignInPage.EMAIL_INPUT, "wrong@gmail.com");
+        sendText(SignInPage.PASSWORD_INPUT, "Mona3yo#");
+    }
+    @Then("should see an error message {string}")
+    public void errorMassage(){
+        String actualErrorMessage = getElementText(SignInPage.ERROR_MESSAGE);
+        String expectedErrorMessage = "wrong username or password";
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+}
--- src/test/resources/features/SecurityTest.feature
+++ src/test/resources/features/SecurityTest.feature
@@ -1,8 +1,6 @@
-Feature: Security Test
+Feature: User Story 1
   Scenario: User signin with valid username and password
-    Given Open browser navigate to retail app
     Then click on SignIn button
-    When input the validate username and password
-    And click on SignIn button
-    Then validate the Account link from main page
-    Then Close  browser
+    Then input the validate username and password
+    And click on the login button
+    Then user should be able to see account link
